98a1e99fd23ebc829a8b93cbe0efd3284f7c7a1b
coder-ms <marco.sbrascia@gmail.com>
2022-10-12 16:18:37 +0300

    aggiustamenti es 2 pt3

--- js/script-snack2.js
+++ js/script-snack2.js
@@ -1,19 +1,23 @@
+
+/*Array di nomi e cognomi per creare invitati*/
 var namePerson = ["Charles", "Nick", "Daisy", "Tom", "Jordan", "Myrtle", "George", "Meye", "Walter",
  "Henry", "Catherine", "August", "James", "Fred", "Laura", "William","Elizabth"];
 var surnamePerson = ["Phillis", "Carraway", "Maguire", "Thompson", "Clarke", "Buchanan", "Fisher",
  "Lewis", "Weiss", "Falk", "Mulvany", "Landsbury", "Stepson", "Simpson", "Mulligan", "Smith","Jackson"];
 
- var listHost = [];
+/*Array da riempire con gli invitati creati*/
+var listHost = [];
 
 console.log(namePerson);
 console.log(surnamePerson);
 
+/*Collegamento bottone genera(HTML) alla funzione JS*/
 const bottone = document.getElementById('btnGenerate');
 bottone.addEventListener('click', getPartyHost);
 
 function getPartyHost() {
+    /*Creo randomicamente gli invitati*/
     var completeName = namePerson[Math.floor(Math.random() * namePerson.length)] + ' ' + surnamePerson[Math.floor(Math.random() * surnamePerson.length)];
-    listHost.push(completeName);
 
     listHost = document.querySelector('.list').value;
     document.querySelector('.nameList').innerHTML = '';
@@ -25,4 +29,5 @@ function getPartyHost() {
             document.querySelector('.nameList').innerHTML += completeName + '<br>';
         }
     }
+    listHost.push(completeName);
 }
