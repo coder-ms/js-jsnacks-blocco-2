98ed74da5119939b8a38e6496b0212872c9f7f2f
coder-ms <marco.sbrascia@gmail.com>
2022-10-12 15:46:29 +0300

    base esercizio

---
+++ js/script-snack2.js
@@ -0,0 +1,25 @@
+var namePerson = ["Charles", "Nick", "Daisy", "Tom", "Jordan", "Myrtle", "George", "Meye", "Walter",
+ "Henry", "Catherine", "August", "James", "Fred", "Laura", "William","Elizabth"];
+var surnamePerson = ["Phillis", "Carraway", "Maguire", "Thompson", "Clarke", "Buchanan", "Fisher",
+ "Lewis", "Weiss", "Falk", "Mulvany", "Landsbury", "Stepson", "Simpson", "Mulligan", "Smith","Jackson"];
+
+console.log(namePerson);
+console.log(surnamePerson);
+
+const bottone = document.getElementById('btnGenerate');
+bottone.addEventListener('click', getPartyHost);
+
+function getPartyHost() {
+    var completeName = namePerson[Math.floor(Math.random() * namePerson.length)] + ' ' + surnamePerson[Math.floor(Math.random() * surnamePerson.length)];
+
+    var listHost = document.querySelector('.list').value;
+    document.querySelector('.nameList').innerHTML = '';
+    if(listHost > 10){
+        document.querySelector('.nameList').innerHTML = 'ONLY 10 PEOPLE CAN STAY AT THE PARTY';
+    }
+    else{
+        for(let i = 0; i < listHost; i++){
+            document.querySelector('.nameList').innerHTML += completeName + '<br>';
+        }
+    }
+}
